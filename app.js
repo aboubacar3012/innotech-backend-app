9eefc69bc9772f5d19b408f531a3f642ceced2a4
Aboubacar Diallo <aboubacar.diallo@nojyk.com>
2023-03-30 12:24:46 +0200

    adding cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const middleware = require("./src/utils/middleware");
+const cors = require('cors')
 require("./src/model/dbConnection");
 
 const usersRouter = require("./src/routes/user.routes");
@@ -12,6 +13,7 @@ const batchRouter = require("./src/routes/batch.routes");
 
 const app = express();
 
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
